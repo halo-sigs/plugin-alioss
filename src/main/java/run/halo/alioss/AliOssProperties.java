7e975b0cb2822e560ef92fa4f61a06d596cc2d17
longjuan <769022681@qq.com>
2022-12-16 18:21:36 +0800

    handle the case of domain=null

--- src/main/java/run/halo/alioss/AliOssProperties.java
+++ src/main/java/run/halo/alioss/AliOssProperties.java
@@ -35,10 +35,12 @@ class AliOssProperties {
     }
 
     public void setDomain(String domain) {
-        if (domain.toLowerCase().startsWith("http://")){
-            domain = domain.substring(7);
-        } else if (domain.toLowerCase().startsWith("https://")) {
-            domain = domain.substring(8);
+        if (domain != null){
+            if (domain.toLowerCase().startsWith("http://")){
+                domain = domain.substring(7);
+            } else if (domain.toLowerCase().startsWith("https://")) {
+                domain = domain.substring(8);
+            }
         }
         this.domain = domain;
     }
